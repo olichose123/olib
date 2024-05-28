f80a2e18e27376edf6072f3253a00a7df7758afa
Olivier Banville <oli.banville@gmail.com>
2024-05-28 15:10:41 -0400

    add logger test to runner

--- src/olib/tests/Main.hx
+++ src/olib/tests/Main.hx
@@ -2,13 +2,14 @@ package olib.tests;
 
 import utest.Runner;
 import utest.ui.Report;
+import olib.logging.Logger.LoggerTest;
 
 class Main
 {
     public static function main()
     {
         var runner = new Runner();
-        // runner.addCase(new TestCase1());
+        runner.addCase(new LoggerTest());
         Report.create(runner);
         runner.run();
     }
