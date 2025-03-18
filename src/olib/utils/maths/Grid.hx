1e109ea1b637de8f833ff6f1dde1bbebaf3f065c
Olivier Banville <olivier@banville.net>
2025-03-17 20:19:34 -0400

    fix variable names that were using old flash as3 standards

--- src/olib/utils/maths/Grid.hx
+++ src/olib/utils/maths/Grid.hx
@@ -2,9 +2,9 @@ package olib.utils.maths;
 
 import haxe.Exception;
 
-class Grid<Object>
+class Grid<T>
 {
-    private var mData:Array<Object>;
+    private var data:Array<T>;
 
     /**
      * The 2D grid's width
@@ -16,171 +16,171 @@ class Grid<Object>
     **/
     public var height(default, null):Int = 0;
 
-    private var mMax:Int = 0;
+    private var maxItemCount:Int = 0;
 
     /**
      * Create a new grid with a fixed width and height
-     * @param	aWidth
-     * @param	aHeight
+     * @param	width
+     * @param	height
      * @throws GridError if out of bounds
     **/
-    public function new(aWidth:Int, aHeight:Int):Void
+    public function new(width:Int, height:Int):Void
     {
-        if (aWidth < 1 || aHeight < 1)
+        if (width < 1 || height < 1)
         {
-            throw new GridError('Grid width ($aWidth) and height ($aHeight) should be positive values');
+            throw new GridError('Grid width ($width) and height ($height) should be positive values');
         }
 
-        width = aWidth;
-        height = aHeight;
-        mMax = width * height;
+        this.width = width;
+        this.height = height;
+        maxItemCount = width * height;
 
-        mData = new Array();
+        data = new Array();
     }
 
     /**
      * Fill the grid with a single value
-     * @param aData
+     * @param data
     **/
-    public function fill(aData:Object):Void
+    public function fill(data:T):Void
     {
         for (i in 0...width * height)
         {
-            mData[i] = aData;
+            this.data[i] = data;
         }
     }
 
     /**
      * Returns the value at the specified position
-     * If aY is not set, then aX_or_id is considered a 1D id.
+     * If y is not set, then x_or_id is considered a 1D id.
      * (ex: the value 3 would be the fourth item, and the value at position (3, 0) on a grid of 10,10)
-     * @param	aX_or_id Either a 1D position or the X value of a coordinate
-     * @param	aY If a value, will act as the Y position of a coordinate
+     * @param	x_or_id Either a 1D position or the X value of a coordinate
+     * @param	y If a value, will act as the Y position of a coordinate
      * @return the value present at the specified position
      * @throws GridError if out of bounds
     **/
-    public inline function get(aX_or_id:Int, ?aY:Null<Int>):Object
+    public inline function get(x_or_id:Int, ?y:Null<Int>):T
     {
-        if (aY == null)
+        if (y == null)
         {
-            if (aX_or_id < 0 || aX_or_id > mMax)
-                throw new GridError('Grid value must be between 0 and $mMax (got $aX_or_id)');
+            if (x_or_id < 0 || x_or_id > maxItemCount)
+                throw new GridError('Grid value must be between 0 and $maxItemCount (got $x_or_id)');
 
-            return mData[aX_or_id];
+            return data[x_or_id];
         }
         else
         {
-            return mData[_pos_to_id(aX_or_id, aY)];
+            return data[_pos_to_id(x_or_id, y)];
         }
     }
 
-    public inline function getOrNull(aX_or_id:Int, ?aY:Null<Int>):Object
+    public inline function getOrNull(x_or_id:Int, ?y:Null<Int>):T
     {
-        if (aY == null)
+        if (y == null)
         {
-            if (aX_or_id < 0 || aX_or_id > mMax)
+            if (x_or_id < 0 || x_or_id > maxItemCount)
                 return null;
 
-            return mData[aX_or_id];
+            return data[x_or_id];
         }
         else
         {
-            var i = _pos_to_idOrNull(aX_or_id, aY);
+            var i = _pos_to_idOrNull(x_or_id, y);
             if (i == null)
             {
                 return null;
             }
             else
             {
-                return mData[i];
+                return data[i];
             }
         }
     }
 
     /**
      * Returns the value at the specified position
-     * If aY is not set, then aX_or_id is considered a 1D id.
+     * If y is not set, then x_or_id is considered a 1D id.
      * (ex: the value 3 would be the fourth item, and the value at position (3, 0) on a grid of 10,10)
-     * @param	aObj The object to set at the specified position
-     * @param	aX_or_id Either a 1D position or the X value of a coordinate
-     * @param	aY If a value, will act as the Y position of a coordinate
+     * @param	obj The T to set at the specified position
+     * @param	x_or_id Either a 1D position or the X value of a coordinate
+     * @param	y If a value, will act as the Y position of a coordinate
      * @return The value present before the new one
      * @throws GridError if out of bounds
     **/
-    public inline function set(aObj:Object, aX_or_id:Int, ?aY:Null<Int>):Object
+    public inline function set(obj:T, x_or_id:Int, ?y:Null<Int>):T
     {
-        var old:Object = null;
-        if (aY == null)
+        var old:T = null;
+        if (y == null)
         {
-            if (aX_or_id < 0 || aX_or_id > mMax)
-                throw new GridError('Grid value must be between 0 and $mMax (got $aX_or_id)');
+            if (x_or_id < 0 || x_or_id > maxItemCount)
+                throw new GridError('Grid value must be between 0 and $maxItemCount (got $x_or_id)');
 
-            old = mData[aX_or_id];
-            mData[aX_or_id] = aObj;
+            old = data[x_or_id];
+            data[x_or_id] = obj;
             return old;
         }
         else
         {
-            var i:Int = _pos_to_id(aX_or_id, aY);
-            old = mData[i];
-            mData[i] = aObj;
+            var i:Int = _pos_to_id(x_or_id, y);
+            old = data[i];
+            data[i] = obj;
             return old;
         }
     }
 
     /**
      * Returns the X value of a coordinate based on a 1D position
-     * @param	aId the 1D array position
+     * @param	id the 1D Array position
      * @return the X part of the coordinate represented by the 1D position
      * @throws GridError if out of bounds
     **/
-    public inline function _posx(aId:Int):Int
+    public inline function _posx(id:Int):Int
     {
-        if (aId < 0 || aId > mMax)
-            throw new GridError('Grid value must be between 0 and $mMax (got $aId)');
+        if (id < 0 || id > maxItemCount)
+            throw new GridError('Grid value must be between 0 and $maxItemCount (got $id)');
 
-        return aId % width;
+        return id % width;
     }
 
     /**
      * Returns the y value of a coordinate based on a 1D position
-     * @param	aId the 1D array position
+     * @param	id the 1D Array position
      * @return the Y part of the coordinate represented by the 1D position
      * @throws GridError if out of bounds
     **/
-    public inline function _posy(aId:Int):Int
+    public inline function _posy(id:Int):Int
     {
-        if (aId < 0 || aId > mMax)
-            throw new GridError('Grid value must be between 0 and $mMax (got $aId)');
+        if (id < 0 || id > maxItemCount)
+            throw new GridError('Grid value must be between 0 and $maxItemCount (got $id)');
 
-        return Std.int(aId / width);
+        return Std.int(id / width);
     }
 
     /**
-     * Converts a X,Y coordinate into a 1D array position
-     * @param	aX
-     * @param	aY
-     * @return a 1D array position
+     * Converts a X,Y coordinate into a 1D Array position
+     * @param	x
+     * @param	y
+     * @return a 1D Array position
      * @throws GridError if out of bounds
     **/
-    public inline function _pos_to_id(aX:Int, aY:Int):Int
+    public inline function _pos_to_id(x:Int, y:Int):Int
     {
-        if (aX < 0 || aY < 0 || aX >= width || aY >= height)
-            throw new GridError('Grid (x:$aX, y:$aY) should be within width(0, $width) and height(0, $height)');
+        if (x < 0 || y < 0 || x >= width || y >= height)
+            throw new GridError('Grid (x:$x, y:$y) should be within width(0, $width) and height(0, $height)');
 
-        return aY * width + aX;
+        return y * width + x;
     }
 
-    public inline function _pos_to_idOrNull(aX:Int, aY:Int):Null<Int>
+    public inline function _pos_to_idOrNull(x:Int, y:Int):Null<Int>
     {
-        if (aX < 0 || aY < 0 || aX >= width || aY >= height)
+        if (x < 0 || y < 0 || x >= width || y >= height)
             return null;
 
-        return aY * width + aX;
+        return y * width + x;
     }
 
     /**
-     * Returns a simple, non-representative string representation of this array
+     * Returns a simple, non-representative string representation of this Array
      * in the form of [Grid width height]
      * @return
     **/
@@ -193,90 +193,119 @@ class Grid<Object>
      * Enables looping through the values, from left to right then from up to down
      * @return
     **/
-    public function iterator():Iterator<Object>
+    public function iterator():Iterator<T>
     {
-        return mData.iterator();
+        return data.iterator();
     }
 
-    public function toArray():Array<Object>
+    public function toArray():Array<T>
     {
-        return mData.copy();
+        return data.copy();
     }
 
     /**
-     * Sets this grid's data as a new array of defined elements
-     * @param	aArr an array of Object, of correct size
-     * @throws GridError if array is of wrong length
+     * Sets this grid's data as a new Array of defined elements
+     * @param	arr an Array of T, of correct size
+     * @throws GridError if Array is of wrong length
     **/
-    public function setData(aArr:Array<Object>, ?force:Bool = false):Void
+    public function setData(arr:Array<T>, ?force:Bool = false):Void
     {
-        if (aArr.length == mMax)
+        if (arr.length == maxItemCount)
         {
-            mData = aArr;
-            mMax = mData.length;
+            data = arr;
+            maxItemCount = data.length;
         }
         else
         {
             if (!force)
-                throw new GridError('Grid setData is of wrong size. Should be an array of $mMax long instead of ${aArr.length}');
+                throw new GridError('Grid setData is of wrong size. Should be an Array of $maxItemCount long instead of ${arr.length}');
             else
             {
-                while (aArr.length < mMax)
+                while (arr.length < maxItemCount)
                 {
-                    aArr.push(null);
+                    arr.push(null);
                 }
-                mData = aArr;
-                mMax = mData.length;
+                data = arr;
+                maxItemCount = data.length;
             }
         }
     }
 
     /**
-     * Returns a three by three square representing aX aY as the middle value
+     * Returns a three by three square representing x y as the middle value
      * and its neighbors. Values out of bounds are null.
-     * @param aX
-     * @param aY
-     * @return Array<Object>
+     * @param x
+     * @param y
+     * @return Array<T>
     **/
-    public function getNeighbors(aX:Int, aY:Int):Array<Object>
+    public function getNeighbors(x:Int, y:Int):Array<T>
     {
-        var result:Array<Object> = [];
+        var result:Array<T> = [];
 
-        result.push(getOrNull(aX - 1, aY - 1));
-        result.push(getOrNull(aX, aY - 1));
-        result.push(getOrNull(aX + 1, aY - 1));
-        result.push(getOrNull(aX - 1, aY));
-        result.push(getOrNull(aX, aY));
-        result.push(getOrNull(aX + 1, aY));
-        result.push(getOrNull(aX - 1, aY + 1));
-        result.push(getOrNull(aX, aY + 1));
-        result.push(getOrNull(aX + 1, aY + 1));
+        result.push(getOrNull(x - 1, y - 1));
+        result.push(getOrNull(x, y - 1));
+        result.push(getOrNull(x + 1, y - 1));
+        result.push(getOrNull(x - 1, y));
+        result.push(getOrNull(x, y));
+        result.push(getOrNull(x + 1, y));
+        result.push(getOrNull(x - 1, y + 1));
+        result.push(getOrNull(x, y + 1));
+        result.push(getOrNull(x + 1, y + 1));
 
         return result;
     }
 
     /**
      * Returns a new grid consisting of a rectangular part of this grid.
-     * The new grid contains 1D ids to this grid's objects
-     * @param	aX
-     * @param	aY
-     * @param	aWidth
-     * @param	aHeight
+     * The new grid contains 1D ids to this grid's Ts
+     * @param	x
+     * @param	y
+     * @param	width
+     * @param	height
      * @return
     **/
-    public function getSegment(aX:Int, aY:Int, aWidth:Int, aHeight:Int):Grid<Int>
+    public function getSegmentIds(x:Int, y:Int, width:Int, height:Int):Grid<Int>
     {
-        if (aX < 0 || aX >= width || aY < 0 || aY >= height || aWidth > width || aHeight > height)
+        if (x < 0 || x >= width || y < 0 || y >= height || width > width || height > height)
             throw new GridError('Grid getSegment is larger than actual grid or out of bounds');
 
-        var g:Grid<Int> = new Grid(aWidth, aHeight);
+        var g:Grid<Int> = new Grid(width, height);
         var a:Array<Int> = [];
 
-        for (k in aY...(aY + aHeight))
+        for (k in y...(y + height))
         {
-            for (j in aX...(aX + aWidth))
+            for (j in x...(x + width))
             {
                 a.push(_pos_to_id(j, k));
+            }
+        }
+
+        g.setData(a);
+        return g;
+    }
+
+    /**
+     * Returns a new grid consisting of a rectangular part of this grid.
+     * The new grid contains Ts
+     * @param x
+     * @param y
+     * @param width
+     * @param height
+     * @return Grid<T>
+    **/
+    public function getSegment(x:Int, y:Int, width:Int, height:Int):Grid<T>
+    {
+        if (x < 0 || x >= width || y < 0 || y >= height || width > width || height > height)
+            throw new GridError('Grid getSegment is larger than actual grid or out of bounds');
+
+        var g:Grid<T> = new Grid(width, height);
+        var a:Array<T> = [];
+
+        for (k in y...(y + height))
+        {
+            for (j in x...(x + width))
+            {
+                a.push(get(j, k));
             }
         }
 
